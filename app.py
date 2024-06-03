131db5aeb13d822b51d738bab09e0fedc0aec67a
BrightLuminate <159859713+BrightLuminate@users.noreply.github.com>
2024-06-03 22:41:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
-def create_app() :
-
+def create_app():
     app = Flask(__name__)
 
-    from myapp import views
-    app.register_blueprint(views.myapp, url_prefix="/myapp")
-  
+    from myapp.views import myapp
+    app.register_blueprint(myapp, url_prefix="/myapp")
+
+    @app.route('/')
+    def root():
+        return redirect(url_for('myapp.index'))
+
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
